ef17f29cf3ad9076dbfd78169c115891e97a1575
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-06-10 23:39:53 -0500

    clap process args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Clap, AppSettings};
+
+#[derive(Clap, Debug)]
+#[clap(version = "1.0", author = "Andrew Gazelka")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+
+    host: String,
+
+    #[clap(short,long, default_value = "1")]
+    count: usize,
+
+    #[clap(short, long)]
+    online: bool,
+
+    #[clap(short, long)]
+    socks5: bool,
+}
+
 fn main() {
     println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    println!("opts {:?}", opts);
 }
