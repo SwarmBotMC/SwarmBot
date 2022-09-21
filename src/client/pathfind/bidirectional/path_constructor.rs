3714c00cb98fb25237915ce120894a0b1d44afd5
Andrew Gazelka <andrew.gazelka@gmail.com>
2022-09-21 01:57:47 -0700

    remove copyright

--- src/client/pathfind/bidirectional/path_constructor.rs
+++ src/client/pathfind/bidirectional/path_constructor.rs
@@ -1,24 +1,6 @@
-/*
- * Copyright (c) 2021 Andrew Gazelka - All Rights Reserved.
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-use std::collections::HashMap;
-use std::hash::Hash;
+use std::{collections::HashMap, hash::Hash};
 
 pub struct PathConstructor;
-
 
 fn path_trace<T: Copy + Hash + Eq>(from: T, lookup: &HashMap<T, T>, into: &mut Vec<T>) {
     let mut on = from;
@@ -30,7 +12,11 @@ fn path_trace<T: Copy + Hash + Eq>(from: T, lookup: &HashMap<T, T>, into: &mut V
 }
 
 impl PathConstructor {
-    pub fn build_path<T: Copy + Hash + Eq>(forward: &HashMap<T, T>, backward: &HashMap<T, T>, split: T) -> Vec<T> {
+    pub fn build_path<T: Copy + Hash + Eq>(
+        forward: &HashMap<T, T>,
+        backward: &HashMap<T, T>,
+        split: T,
+    ) -> Vec<T> {
         let mut vec = Vec::new();
         path_trace(split, forward, &mut vec);
         vec.reverse();
